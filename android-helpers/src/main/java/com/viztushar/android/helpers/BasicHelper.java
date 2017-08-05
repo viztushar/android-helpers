f26f827a319ff418e1c37607f1553e7c89f65d4f
Archit Jain <architjn93@gmail.com>
2017-08-05 17:35:39 +0530

    Added More Functions

---
+++ android-helpers/src/main/java/com/viztushar/android/helpers/BasicHelper.java
@@ -0,0 +1,44 @@
+package com.viztushar.android.helpers;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * Created by HP on 05-08-2017.
+ */
+
+public class BasicHelper {
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
+    public static void generateHashKey(Context context) {
+        MessageDigest md = null;
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+            }
+        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Log.i("SecretKey = ", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+    }
+
+    public static String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        return sb.toString();
+    }
+}
